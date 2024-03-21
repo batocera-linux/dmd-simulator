0e42c32c1cd7f0795110319c57f5afa5640391eb
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2024-03-21 20:45:14 +0100

    fix: disconnect
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- dmd-play.py
+++ dmd-play.py
@@ -240,7 +240,7 @@ class DmdPlayer:
         if args.overlay:
             time.sleep(args.overlay_time/1000)
 
-        client.disconnect()
+        client.close()
 
 if __name__ == '__main__':
     DmdPlayer.run(feature_video)
